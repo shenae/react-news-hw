d0a149fc09476f3f9c6763e9a521c70973950ec9
Shenae Simmons <shhnae@gmail.com>
2019-04-16 22:54:01 -0400

    set up 2 components

---
+++ news-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import travelNews from './components/travelNews/travelNews.js';
+import AOCNews from './components/AOCNews/AOCNews.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
